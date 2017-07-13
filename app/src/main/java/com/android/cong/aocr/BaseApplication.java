2512696952c5e799154819458e5dedbde5983e45
xiaokecong <xiaokecong@baidu.com>
2017-07-13 19:44:48 +0800

    baidu ocr 获取access token
    可通过initAccessToken方法获取，将授权文件aip.license文件放到assets文件夹下，这样可隐藏api_key和secret_key，毕竟安全

--- app/src/main/java/com/android/cong/aocr/BaseApplication.java
+++ app/src/main/java/com/android/cong/aocr/BaseApplication.java
@@ -1,5 +1,6 @@
 package com.android.cong.aocr;
 
+import com.android.cong.aocr.utils.Constant;
 import com.baidu.ocr.sdk.OCR;
 import com.baidu.ocr.sdk.OnResultListener;
 import com.baidu.ocr.sdk.exception.OCRError;
@@ -12,22 +13,22 @@ import android.app.Application;
  */
 
 public class BaseApplication extends Application {
-    private final String API_KEY = "YOUR_API_KEY";
-    private final String SECRET_KEY = "YOUR_SECRET_KEY";
     @Override
     public void onCreate() {
         super.onCreate();
+//        OCR.getInstance().initAccessTokenWithAkSk(new OnResultListener<AccessToken>() {
+//            @Override
+//            public void onResult(AccessToken result) {
+//                // 调用成功，返回AccessToken对象
+//                String token = result.getAccessToken();
+//            }
+//            @Override
+//            public void onError(OCRError error) {
+//                // 调用失败，返回OCRError子类SDKError对象
+//            }
+//        }, getApplicationContext(), BAIDU_API_KEY, BAIDU_SECRET_KEY);
 
-        OCR.getInstance().initAccessTokenWithAkSk(new OnResultListener<AccessToken>() {
-            @Override
-            public void onResult(AccessToken result) {
-                // 调用成功，返回AccessToken对象
-                String token = result.getAccessToken();
-            }
-            @Override
-            public void onError(OCRError error) {
-                // 调用失败，返回OCRError子类SDKError对象
-            }
-        }, getApplicationContext(), API_KEY, SECRET_KEY);
+        // 使用access token 进行ocr初始化
+        OCR.getInstance().initWithToken(getApplicationContext(), Constant.BAIDU_ACCESS_TOKEN);
     }
 }
